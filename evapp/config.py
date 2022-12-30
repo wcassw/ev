bcdf6d7da6dea1fcc95efa7ea17f7f11d3fe91a9
wcassw <wcassw@ymail.com>
2022-12-30 21:47:26 +0000

    Update config.py

--- evapp/config.py
+++ evapp/config.py
@@ -1,9 +1,9 @@
 from urllib.parse import quote
 from sqlalchemy.engine import create_engine
+import os
 
 class BaseConfig(object):
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/uploads'  # changed to relative path
     TEMPLATE = 'static/template'  # changed to relative path
     MAIL_USE_TLS = False
@@ -15,6 +15,12 @@ class BaseConfig(object):
     SQLAlCHEMY_ECHO = False
     DEBUG = False
     APP_SETTINGS=''
+    
+    os.environ["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    os.environ["MAIL_USE_SSL"] = True
+    os.environ["DBNAME"] = 'template.db'
+    os.environ["DEBUG"] = False
+    os.environ["APP_SETTINGS"] = ''
 
 
 class DevelopmentConfig(BaseConfig):
