bb5422b5542a555022337e64e0de397bc90175c4
wcassw <wcassw@ymail.com>
2022-12-30 22:28:01 +0000

    Update forms.py

--- evapp/users/forms.py
+++ evapp/users/forms.py
@@ -1,14 +1,23 @@
-from flask_wtf import Form
-from wtforms import TextField, PasswordField
+from flask_wtf import FlaskForm
+from wtforms import TextField, PasswordField, StringField, validators, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+import email_validator
+  
+  
+class contactForm(FlaskForm):
+    name = StringField(label='Name', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[
+      DataRequired(), Email(granular_message=True)])
+       message= StringField(label='Message')
+    submit = SubmitField(label="Log In")
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = TextField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
 
 
-class RegisterForm(Form):
+class RegisterForm(FlaskForm):
     username = TextField(
         'username',
         validators=[DataRequired(), Length(min=3, max=25)]
