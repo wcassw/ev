90fd70c7a2711a9ca760313c2caf9e1e1b5acc1e
wcassw <wcassw@ymail.com>
2022-12-31 01:27:18 +0000

    Update forms.py

--- evapp/users/forms.py
+++ evapp/users/forms.py
@@ -34,3 +34,39 @@ class RegisterForm(Form):
             DataRequired(), EqualTo('password', message='Passwords must match.')
         ]
     )
+
+
+class LoginForm2(FlaskForm):
+    """Login form."""
+
+    email = EmailField(
+        "Email Address", validators=[DataRequired(), Email(), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        "Password", validators=[DataRequired()]
+    )
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(LoginForm, self).validate()
+        if not initial_validation:
+            return False
+
+        self.user = User.query.filter_by(email=self.email.data).first()
+        if not self.user:
+            self.email.errors.append("Unknown email address!")
+            return False
+
+        if not self.user.check_password(self.password.data):
+            self.password.errors.append("Invalid password!")
+            return False
+
+        if not self.user.verified:
+            self.email.errors.append("Please verify your email address!")
+            return False
+        return True
