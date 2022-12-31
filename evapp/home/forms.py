d95b29ebf8f697fcdd245ea3ad32abe01daadd3b
wcassw <wcassw@ymail.com>
2022-12-31 00:36:01 +0000

    Update forms.py

--- evapp/home/forms.py
+++ evapp/home/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import Form
 from wtforms import TextField
 from wtforms.validators import DataRequired, Length
+from werkzeug.urls import url_encode   # pragma: no cover
+import bcrypt
 
 class MessageForm(Form):
     title = TextField('Title', validators=[DataRequired()])
