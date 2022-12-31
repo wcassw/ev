610cb5d490ecb13eeeb4010630cab9e3f2bf669c
wcassw <wcassw@ymail.com>
2022-12-31 01:30:25 +0000

    Update forms.py

--- evapp/users/forms.py
+++ evapp/users/forms.py
@@ -35,7 +35,7 @@ class RegisterForm(Form):
         ]
     )
 
-
+"""
 class LoginForm2(FlaskForm):
     """Login form."""
 
@@ -70,3 +70,80 @@ class LoginForm2(FlaskForm):
             self.email.errors.append("Please verify your email address!")
             return False
         return True
+
+# -------
+
+from wtforms.widgets import PasswordInput, CheckboxInput, TextInput
+from wtforms.widgets.html5 import EmailInput
+
+
+class BootstrapVerifyEmail(EmailInput):
+    """Bootstrap Validator for email"""
+
+    def __init__(self, error_class=u"is-invalid"):
+        super(BootstrapVerifyEmail, self).__init__()
+        self.error_class = error_class
+
+    def __call__(self, field, **kwargs):
+        if field.errors:
+            c = kwargs.pop("class", "") or kwargs.pop("class_", "")
+            kwargs["class"] = u"%s %s" % (self.error_class, c)
+        return super(BootstrapVerifyEmail, self).__call__(field, **kwargs)
+
+
+class BootstrapVerifyPassword(PasswordInput):
+    """Bootstrap Validator for password"""
+
+    def __init__(self, error_class=u"is-invalid"):
+        super(BootstrapVerifyPassword, self).__init__()
+        self.error_class = error_class
+
+    def __call__(self, field, **kwargs):
+        if field.errors:
+            c = kwargs.pop("class", "") or kwargs.pop("class_", "")
+            kwargs["class"] = u"%s %s" % (self.error_class, c)
+        return super(BootstrapVerifyPassword, self).__call__(field, **kwargs)
+
+
+class BootstrapVerifyBoolean(CheckboxInput):
+    """Bootstrap Validator for boolean"""
+
+    def __init__(self, error_class=u"is-invalid"):
+        super(BootstrapVerifyBoolean, self).__init__()
+        self.error_class = error_class
+
+    def __call__(self, field, **kwargs):
+        if field.errors:
+            c = kwargs.pop("class", "") or kwargs.pop("class_", "")
+            kwargs["class"] = u"%s %s" % (self.error_class, c)
+        return super(BootstrapVerifyBoolean, self).__call__(field, **kwargs)
+
+
+class BootstrapVerifyText(TextInput):
+    """Bootstrap Validator for text"""
+
+    def __init__(self, error_class=u"is-invalid"):
+        super(BootstrapVerifyText, self).__init__()
+        self.error_class = error_class
+
+    def __call__(self, field, **kwargs):
+        if field.errors:
+            c = kwargs.pop("class", "") or kwargs.pop("class_", "")
+            kwargs["class"] = u"%s %s" % (self.error_class, c)
+        return super(BootstrapVerifyText, self).__call__(field, **kwargs)
+
+html
+{{ login_user_form.email.label(class_="form-control-label") }}
+<div class="input-group input-group-merge">
+    <div class="input-group-prepend">
+        <span class="input-group-text" id="user"><i class="fas fa-user"></i></span>
+    </div>
+    {{ login_user_form.email(placeholder="name@example.com", class_="form-control", **{"aria-describedby": "inputGroupPrepend3", "required": ""}) }}
+    {% for error in login_user_form.email.errors %}
+        <div class="invalid-feedback">{{ error }}</div>
+    {% endfor %}
+</div>
+
+
+
+"""
